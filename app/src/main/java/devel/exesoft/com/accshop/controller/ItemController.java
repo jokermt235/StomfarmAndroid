ca126be82cfc888ec7eac9a7b819df0b671339ba
mutalip <jokermt2357@gmail.com>
2019-03-29 18:35:15 +0600

    Items controller

--- app/src/main/java/devel/exesoft/com/accshop/controller/ItemController.java
+++ app/src/main/java/devel/exesoft/com/accshop/controller/ItemController.java
@@ -8,6 +8,8 @@ import com.android.volley.VolleyError;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+
 import devel.exesoft.com.accshop.R;
 import devel.exesoft.com.accshop.model.Item;
 import devel.exesoft.com.accshop.model.Store;
@@ -23,7 +25,7 @@ public class ItemController extends AppController {
 
     public static void getItemByBarcode(String barcode)
     {
-        String url = getInstance().getString(R.string.server_url) + "/" + NAME;
+        String url = getInstance().getString(R.string.server_url) + "/" + NAME + "/"  + "relocation";
 
         JSONObject params  = new JSONObject();
         final Realm realm = Realm.getDefaultInstance();
@@ -31,52 +33,56 @@ public class ItemController extends AppController {
         try {
             if(user != null) {
                 params.put("barcode", barcode);
-                url += "?token=" + user.getToken() + "&barcode=" + barcode;
+                params.put("token", user.getToken());
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        CustomStringRequest jsonObjectRequest = new CustomStringRequest(url, params, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.e(TAG, response);
-                if(response != null){
-                    try {
-                        JSONObject result = new JSONObject(response);
+        try {
+            CustomStringRequest jsonObjectRequest = new CustomStringRequest(url, params, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.e(TAG, response);
+                    if (response != null) {
+                        try {
+                            JSONObject result = new JSONObject(response);
 
-                        if(result.getBoolean("success")){
-                            final Item item = new Item();
-                            JSONObject data = result.getJSONObject("data");
+                            if (result.getBoolean("success")) {
+                                final Item item = new Item();
+                                JSONObject data = result.getJSONObject("data");
 
-                            Log.d(TAG, data.toString());
-                            item.setName(data.getString("name"));
-                            item.setStore_id(data.getLong("storage_id"));
-                            item.setAcc_code(data.getString("acc_code"));
+                                Log.d(TAG, data.toString());
+                                item.setName(data.getString("name"));
+                                item.setStore_id(data.getLong("storage_id"));
+                                item.setAcc_code(data.getString("acc_code"));
 
-                            realm.executeTransaction(new Realm.Transaction() {
-                                @Override
-                                public void execute(Realm realm) {
-                                    // This will create a new object in Realm or throw an exception if the
-                                    // object already exists (same primary key)
-                                    realm.copyToRealm(item);
-                                    //activityStorageBinding.storageToolbar.setTitle(store.getName());
-                                }
-                            });
-                            realm.close();
+                                realm.executeTransaction(new Realm.Transaction() {
+                                    @Override
+                                    public void execute(Realm realm) {
+                                        // This will create a new object in Realm or throw an exception if the
+                                        // object already exists (same primary key)
+                                        realm.copyToRealm(item);
+                                        //activityStorageBinding.storageToolbar.setTitle(store.getName());
+                                    }
+                                });
+                                realm.close();
+                            }
+                        } catch (Exception excp) {
+                            Log.e(TAG, excp.toString());
                         }
-                    }catch (Exception excp){
-                        Log.e(TAG, excp.toString());
                     }
                 }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, error.toString());
-            }
-        });
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
 
-        getInstance().getRequestQueue().add(jsonObjectRequest);
+            getInstance().getRequestQueue().add(jsonObjectRequest);
+        }catch (UnsupportedEncodingException e){
+            Log.d(TAG, e.getStackTrace().toString());
+        }
     }
 }
--- app/src/main/java/devel/exesoft/com/accshop/view/CustomStringRequest.java
+++ app/src/main/java/devel/exesoft/com/accshop/view/CustomStringRequest.java
@@ -6,9 +6,13 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import devel.exesoft.com.accshop.model.User;
@@ -16,6 +20,7 @@ import io.realm.Realm;
 
 public class CustomStringRequest extends StringRequest {
 
+    private static String TAG = "CustomStringRequest";
     private JSONObject body;
     public void setBody(JSONObject pBody){
         body = pBody;
@@ -26,10 +31,16 @@ public class CustomStringRequest extends StringRequest {
         body = params;
     }
 
-    public CustomStringRequest(String url,JSONObject params, Response.Listener<String> listener, Response.ErrorListener errorListener) {
-        super(Method.GET, url, listener, errorListener);
+    public CustomStringRequest(String url,JSONObject params, Response.Listener<String> listener, Response.ErrorListener errorListener) throws UnsupportedEncodingException{
+        super(Method.GET, url + getParamsString(params), listener, errorListener);
+    }
 
-        body = params;
+
+    private String getUrlParams(String url , JSONObject params){
+        if(params != null){
+
+        }
+        return url;
     }
 
     public CustomStringRequest(String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
@@ -53,5 +64,29 @@ public class CustomStringRequest extends StringRequest {
     public byte[] getBody() throws AuthFailureError{
 
         return body.toString().getBytes();
+    }
+
+
+    private static String getParamsString(JSONObject params)
+            throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+
+        try {
+
+            for (Iterator<String> it = params.keys(); it.hasNext(); ) {
+                Object entry = it.next();
+                result.append(URLEncoder.encode(params.get(entry.toString()).toString(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(params.get(entry.toString()).toString(), "UTF-8"));
+                result.append("&");
+            }
+        }catch (JSONException e){
+            Log.d(TAG, e.getStackTrace().toString());
+        }
+
+        String resultString = "?" + result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1)
+                : resultString;
     }
 }
